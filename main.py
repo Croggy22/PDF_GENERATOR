de5da7ef1bffa06463958ca139456a98409a8a9c
Craig F <crogdev@gmail.com>
2022-06-21 09:03:15 -0600

    Update main.py
    Format spacing to align comment with proper code block

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ CSV_OUTPUT_PATH = config('CSV_OUTPUT_PATH')
 # Create empty list to store file names into
 names = []
 files_to_create = input("How may files do you want to create? ")
+
 # Loop over range from input
 # Create files with UUID to ensure uniqueness, append each to names[]
 # Output each file to dir
 # Open and draw text at coords from random lorem sentence then save back to dir
-
 for i in range(int(files_to_create)):
     name = f"Test-{uuid.uuid4()}.pdf"
     names.append(name)
